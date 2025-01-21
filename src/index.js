d51109c081862b580032c42ef7a41f3f0b088760
fabio <fabio@arsenalbio.com>
2025-01-21 15:02:44 -0800

    chore: bumped versions to 1.0.5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Worksheet as WS, Spreadsheet as SP } from '@jspreadsheet-ce/react';
 
-export const Worksheet = React.forwardRef((props, ref)=> <WS {...props} ref={ref} />
+export const Worksheet = React.forwardRef((props, ref)=> <WS {...props} ref={ref} />);
 
-export const Spreadsheet = React.forwardRef((props, ref)=> <SP {...props} ref={ref} />
+export const Spreadsheet = React.forwardRef((props, ref)=> <SP {...props} ref={ref} />);
