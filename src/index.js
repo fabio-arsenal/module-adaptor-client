648337ac247e874aa978c2a9679170412d1c64a6
fabio <fabio@arsenalbio.com>
2025-01-21 14:24:05 -0800

    chore: bumped versions to 1.0.2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import spreadsheet from '@jspreadsheet-ce/react';
+import JSReact from '@jspreadsheet-ce/react';
 
-export const  jspreadsheet = () => spreadsheet;
+export const  Spreadsheet = () => JSReact.Spreadsheet;
+export const  Worksheet = () => JSReact.Worksheet;
+export const JSpreadsheet = {
+    Spreadsheet: JSReact.Spreadsheet,
+    Worksheet: JSReact.Worksheet
+}
+
+const JSpreadsheetReact = {
+    Spreadsheet: JSReact.Spreadsheet,
+    Worksheet: JSReact.Worksheet,
+    JSpreadsheet,
+}
+
+export default JSpreadsheetReact;
